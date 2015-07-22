26692160820d9591f69edc45ff95467b5c10d0ab
Will Floyd <floyd.will@gmail.com>
2015-07-21 20:19:36 -0700

    smarter dumb image fetching

--- examples/infinite-scroll/infinite-scroll.js
+++ examples/infinite-scroll/infinite-scroll.js
@@ -1,21 +1,21 @@
 riot.tag('infinite-scroll', '<div id="loading" if="{ isLoading }"> <span>loading...</span> </div> <div id="page-wrapper"> <img each="{ images }" riot-src="{ this.src }"> </div> <waypoint if="{ !isLoading }" onenter="{ fetchImages }" threshold="{ 0 }"></waypoint>', function(opts) {
     var self = this;
     self.isLoading = false;
-
-    self.images = [
-      { src: 'http://lorempixel.com/640/480/animals/' },
-      { src: 'http://lorempixel.com/640/480/nature/' },
-      { src: 'http://lorempixel.com/640/480/fashion/' },
-      { src: 'http://lorempixel.com/640/480/abstract/' },
-      { src: 'http://lorempixel.com/640/480/cats/' }
-    ];
+    self.images = [];
 
     this.fetchImages = function(e) {
       self.isLoading = true;
       self.update();
 
       window.setTimeout(function() {
-        self.images = self.images.concat(self.images.slice(0, 5));
+        self.images = self.images.concat([
+          { src: 'http://lorempixel.com/640/480/animals/' },
+          { src: 'http://lorempixel.com/640/480/nature/' },
+          { src: 'http://lorempixel.com/640/480/fashion/' },
+          { src: 'http://lorempixel.com/640/480/abstract/' },
+          { src: 'http://lorempixel.com/640/480/cats/' }
+        ]);
+
         self.isLoading = false;
         self.update();
       }, 1200);
--- examples/infinite-scroll/infinite-scroll.tag
+++ examples/infinite-scroll/infinite-scroll.tag
@@ -12,14 +12,7 @@
   <script>
     var self = this;
     self.isLoading = false;
-
-    self.images = [
-      { src: 'http://lorempixel.com/640/480/animals/' },
-      { src: 'http://lorempixel.com/640/480/nature/' },
-      { src: 'http://lorempixel.com/640/480/fashion/' },
-      { src: 'http://lorempixel.com/640/480/abstract/' },
-      { src: 'http://lorempixel.com/640/480/cats/' }
-    ];
+    self.images = [];
 
     fetchImages(e) {
       self.isLoading = true;
@@ -27,7 +20,14 @@
 
       // and then do ajax stuff or whatever
       window.setTimeout(function() {
-        self.images = self.images.concat(self.images.slice(0, 5));
+        self.images = self.images.concat([
+          { src: 'http://lorempixel.com/640/480/animals/' },
+          { src: 'http://lorempixel.com/640/480/nature/' },
+          { src: 'http://lorempixel.com/640/480/fashion/' },
+          { src: 'http://lorempixel.com/640/480/abstract/' },
+          { src: 'http://lorempixel.com/640/480/cats/' }
+        ]);
+
         self.isLoading = false;
         self.update();
       }, 1200);
